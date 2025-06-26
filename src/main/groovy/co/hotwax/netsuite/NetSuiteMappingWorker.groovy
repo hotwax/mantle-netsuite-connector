d47a096068275b7df26e89fa42c5fdc264294cb4
nutan shinde <nutan.shinde@hotwax.co>
2025-06-26 20:59:25 +0530

    Added workerClass and improve the generate#createOrderFeed remove preapre#OrdersFeedDataForNetSuite and add#BusinessLogicInFeedData

---
+++ src/main/groovy/co/hotwax/netsuite/NetSuiteMappingWorker.groovy
@@ -0,0 +1,65 @@
+package co.hotwax.netsuite
+
+import org.moqui.context.ExecutionContext
+import org.moqui.entity.EntityValue
+
+class NetSuiteMappingWorker {
+    /**
+     * Returns the mapped NetSuite value from IntegrationTypeMapping
+     * based on the given integrationTypeId and mappingKey.
+     *
+     * @param ec ExecutionContext
+     * @param integrationTypeId e.g., 'NETSUITE_SHP_MTHD'
+     * @param mappingKey e.g., '2_DAY_SHIPPING_67G'
+     * @return Mapped NetSuite value or null
+     */
+    static String getIntegrationMappingValue(ExecutionContext ec, String integrationTypeId, String mappingKey) {
+        if (!integrationTypeId || !mappingKey) return null
+
+        EntityValue mapping = ec.entity.find("co.hotwax.integration.IntegrationTypeMapping")
+            .condition([integrationTypeId: integrationTypeId, mappingKey: mappingKey])
+            .useCache(true)
+            .one()
+        return mapping?.mappingValue
+    }
+
+    /**
+     * Returns the NetSuite product ID for the given HC product ID,
+     * using GoodIdentification with type 'NETSUITE_PRODUCT_ID'.
+     *
+     * @param ec ExecutionContext
+     * @param hcProductId HotWax Commerce product ID
+     * @return NetSuite product ID or null
+     */
+    static String getNetSuiteProductId(ExecutionContext ec, String hcProductId) {
+        if (!hcProductId) return null
+        EntityValue gid = ec.entity.find("mantle.product.good.GoodIdentification")
+            .condition([productId: hcProductId, goodIdentificationTypeEnumId: "NETSUITE_PRODUCT_ID"])
+            .useCache(true)
+            .filterByDate("fromDate", "thruDate", ec.user.nowTimestamp)
+            .one()
+        return gid?.idValue
+    }
+
+    /**
+     * Gets facility identification values for a specific facility ID and identification type.
+     *
+     * @param ec ExecutionContext
+     * @param facilityId Facility ID to get identification for
+     * @param facilityIdenTypeId Type of facility identification (e.g., 'ORDR_ORGN_SLS_CHNL')
+     * @return identification values matching the criteria
+     */
+    static String getFacilityIdentifications(ExecutionContext ec, String facilityId, String facilityIdenTypeId) {
+        if (!facilityId || !facilityIdenTypeId) return []
+
+        EntityValue identifications = ec.entity.find("co.hotwax.facility.FacilityIdentification")
+            .condition("facilityId", facilityId)
+            .condition("facilityIdenTypeId", facilityIdenTypeId)
+            .useCache(true)
+            .filterByDate("fromDate", "thruDate", ec.user.nowTimestamp)
+            .one()
+
+        return identifications?.idValue
+    }
+
+}
